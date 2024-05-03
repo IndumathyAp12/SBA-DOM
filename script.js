1b399f1a5e16f3d1c800f74be94ea15807f6ef63
Indu Prabha <apindhuece12@gmail.com>
2024-05-02 20:18:18 -0400

    DOM function is implemented for tilebuild up

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Memory Game")
 const tilesContainer =  document.querySelector(".tiles");
-const  animals = ['leopard', 'monkey', 'tiger','lion','elephant','crocodile','fox','deer','zebra'];
+const  animals = ['leopard', 'monkey', 'tiger','lion','elephant','crocodile','fox','deer','zebra','rhino','hippo','bear'];
 const  animalsPicklist = [...animals,...animals];
 const tileCount = animalsPicklist.length;
 
@@ -9,11 +9,23 @@ let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
 
+function buildTile(animal) {
+    const element =document.createElement("div");
+
+    element.classList.add("tile");
+    element.setAttribute("data-animal", animal);
+    return element;
+}
+
+
 // Build up tiles
 
 for (let i = 0; i < tileCount; i++) {
     const randomIndex = Math.floor(Math.random() * animalsPicklist.length);
     const animals = animalsPicklist[randomIndex];
+const tile = buildTile(animals);
+
     animalsPicklist.splice(randomIndex, 1);
-    console.log(animals);
+    tilesContainer.appendChild(tile);
+    
 }
