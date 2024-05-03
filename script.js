6bb93b770d137f6df36f7bbf3f81c4df5509c599
Indu Prabha <apindhuece12@gmail.com>
2024-05-02 21:45:31 -0400

    game logic is done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Memory Game")
-const tilesContainer =  document.querySelector(".tiles");
-const  animals = ['leopard', 'monkey', 'tiger','lion','elephant','crocodile','fox','deer','zebra','rhino','hippo','bear'];
-const  animalsPicklist = [...animals,...animals];
+const tilesContainer = document.querySelector(".tiles");
+const animals = ['leopard', 'monkey', 'tiger', 'lion', 'elephant', 'crocodile', 'fox', 'deer', 'zebra', 'rhino', 'hippo', 'bear'];
+const animalsPicklist = [...animals, ...animals];
 const tileCount = animalsPicklist.length;
 
 // Game
@@ -10,30 +10,47 @@ let activeTile = null;
 let awaitingEndOfMove = false;
 
 function buildTile(animal) {
-    const element =document.createElement("div");
+    const element = document.createElement("div");
 
     element.classList.add("tile");
     element.setAttribute("data-animal", animal);
     element.setAttribute("data-revealed", "false");
     element.addEventListener("click", () => {
- if (awaitingEndOfMove) {
-    return;
- }
-  element.style.backgroundColor = animal;
-  if (!activeTile) {
-    activeTile = element;
-    return;
-  }
+        const revealed = element.getAttribute("data-revealed");
+        if (awaitingEndOfMove || revealed === "true" || element == activeTile) {
+            return;
+        }
+        element.style.backgroundColor = animal;
+        if (!activeTile) {
+            activeTile = element;
+            return;
+        }
 
-  awaitingEndOfMove = true;
+        const animalToMatch = activeTile.getAttribute("data-animal");
+        if (animalToMatch === animal) {
+            activeTile.setAttribute("data-revealed", "true");
+            element.setAttribute("data-revealed", "true");
+            awaitingEndOfMove = false;
+            activeTile = null;
+            revealedCount += 2;
 
-  setTimeout(() =>{
-    element.style.backgroundColor = null;
-    activeTile.style.backgroundColor = null;
+            if (revealedCount === tileCount) {
+                alert("Congratulations!!! You have won the game.");
+            }
+        }
 
-    awaitingEndOfMove = false;
-    activeTile = null;
-  },1000);
+
+
+
+        awaitingEndOfMove = true;
+
+        setTimeout(() => {
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+
+            awaitingEndOfMove = false;
+            activeTile = null;
+        }, 1000);
 
     });
     return element;
@@ -45,9 +62,9 @@ function buildTile(animal) {
 for (let i = 0; i < tileCount; i++) {
     const randomIndex = Math.floor(Math.random() * animalsPicklist.length);
     const animals = animalsPicklist[randomIndex];
-const tile = buildTile(animals);
+    const tile = buildTile(animals);
 
     animalsPicklist.splice(randomIndex, 1);
     tilesContainer.appendChild(tile);
-    
+
 }
