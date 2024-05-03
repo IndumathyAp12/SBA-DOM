601bd228f9e25d0ce118e6e1697e793e2bd5a253
Indu Prabha <apindhuece12@gmail.com>
2024-05-02 20:08:43 -0400

    variables declared and tiles are built

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("DOM-SBA")
+console.log("Memory Game")
+const tilesContainer =  document.querySelector(".tiles");
+const  animals = ['leopard', 'monkey', 'tiger','lion','elephant','crocodile','fox','deer','zebra'];
+const  animalsPicklist = [...animals,...animals];
+const tileCount = animalsPicklist.length;
+
+// Game
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
+
+// Build up tiles
+
+for (let i = 0; i < tileCount; i++) {
+    const randomIndex = Math.floor(Math.random() * animalsPicklist.length);
+    const animals = animalsPicklist[randomIndex];
+    animalsPicklist.splice(randomIndex, 1);
+    console.log(animals);
+}
